e7d3007850c46cf017a4f6cd6b05220c9ac83be0
Ivan Nieto Lozada <ivan.nieto@kavak.com>
2022-08-26 20:39:23 -0500

    Initial Commit

---
+++ MVCExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCExample
+//
+//  Created by IVAN NIETO on 26/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
